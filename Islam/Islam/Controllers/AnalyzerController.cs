8444d40a87d2aaa85dfcf9078ddc7343be4bee02
Ильяс Муллахметов <IIMullahmetov@gmail.com>
2017-12-01 14:59:00 +0300

    Добавлен ридме файл

--- Islam/Islam/Controllers/AnalyzerController.cs
+++ Islam/Islam/Controllers/AnalyzerController.cs
@@ -2,6 +2,8 @@
 using Islam.DAL.Entities;
 using Islam.Models.Requests;
 using Islam.Models.Responses;
+using System;
+using System.Linq;
 using System.Web.Http;
 
 namespace Islam.Controllers
@@ -17,8 +19,15 @@ namespace Islam.Controllers
 		{
 			context.Vectors.Add(new Vector { Word = request.Text });
 			context.SaveChanges();
-
-			AnalyzeResponse response = new AnalyzeResponse();
+			Random random = new Random();
+			AnalyzeResponse response = new AnalyzeResponse
+			{
+				Items = context.Emotions.Select(e => new AnalyzeResponseItem
+				{
+					Emotion = e.Enum,
+					//Value = random.NextDouble()
+				})
+			};
 			return Ok(response);
 		}
 	}
