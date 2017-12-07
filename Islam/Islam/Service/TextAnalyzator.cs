c472775f90652cfee85879088dc930bf3819faad
Volkov Kolya <007vampir007@gmail.com>
2017-12-07 07:14:59 +0300

    add stopwords

--- Islam/Islam/Service/TextAnalyzator.cs
+++ Islam/Islam/Service/TextAnalyzator.cs
@@ -8,99 +8,102 @@ using Islam.DAL.Entities;
 
 namespace Islam.Service
 {
-	public class TextAnalyzator
-	{
-		private Context context;
+    public class TextAnalyzator
+    {
+        private Context context;
+        public TextAnalyzator(Context context) => this.context = context;
 
-		public TextAnalyzator(Context context) => this.context = context;
-
-		public EmotionalVector Analyze(string text)
-		{
-			List<string> newWords = new List<string>();  
+        public EmotionalVector Analyze(string text)
+        {
+            List<string> newWords = new List<string>();
             List<EmotionalVector> oldEmoVectors = new List<EmotionalVector>();
-			List<string> words = ParseTextByWord(text);
+            List<string> words = ParseTextByWord(text);
+            //delete stop words (change path)
+            words = deleteStopWords(words);
 
             Stemming stem = new Stemming();
-            for (int i=0; i<words.Count; i++ )
+            for (int i = 0; i < words.Count; i++)
             {
-                words[i]=stem.DoStemming(words[i]);
+                words[i] = stem.DoStemming(words[i]);
             }
+
 
             //Attention use at the first run, then delete;
             fixDb();
-            
+
+
             List<EmotionalVector> dbVectors = new List<EmotionalVector>();
             foreach (var w in words)
-			{
-                
+            {
+
                 DAL.Entities.Vector dbvector = context.Vectors.FirstOrDefault(x => x.Word == w);
-				if (dbvector != null)
-				{
-					EmotionalVector emovector = new EmotionalVector(dbvector.Word, dbvector.Priority, dbvector.Joy,
-						dbvector.Trust, dbvector.Fear, dbvector.Surprise, dbvector.Sadness,
-						dbvector.Disgust, dbvector.Anger, dbvector.Anticipation);
+                if (dbvector != null)
+                {
+                    EmotionalVector emovector = new EmotionalVector(dbvector.Word, dbvector.Priority, dbvector.Joy,
+                        dbvector.Trust, dbvector.Fear, dbvector.Surprise, dbvector.Sadness,
+                        dbvector.Disgust, dbvector.Anger, dbvector.Anticipation);
                     dbVectors.Add(emovector);
-					oldEmoVectors.Add(emovector);
-				}
-				else
-				{
-					newWords.Add(w);
-				}
-			}
+                    oldEmoVectors.Add(emovector);
+                }
+                else
+                {
+                    newWords.Add(w);
+                }
+            }
 
             EmotionalVector sum = countSum(text, dbVectors);
 
 
             foreach (EmotionalVector oev in oldEmoVectors)
-			{
-				EmotionalVector oldemovector = oev + sum;
-				DAL.Entities.Vector oldvector = context.Vectors.First(x => x.Word.Equals(oev.VerbalSet));
+            {
+                EmotionalVector oldemovector = oev + sum;
+                DAL.Entities.Vector oldvector = context.Vectors.First(x => x.Word.Equals(oev.VerbalSet));
                 oldvector.Priority = oev.NewPriority(sum);
                 oldvector.Joy = oldemovector.EmotionalTone[0].Value;
-				oldvector.Trust = oldemovector.EmotionalTone[1].Value;
-				oldvector.Fear = oldemovector.EmotionalTone[2].Value;
-				oldvector.Surprise = oldemovector.EmotionalTone[3].Value;
-				oldvector.Sadness = oldemovector.EmotionalTone[4].Value;
-				oldvector.Disgust = oldemovector.EmotionalTone[5].Value;
-				oldvector.Anger = oldemovector.EmotionalTone[6].Value;
-				oldvector.Anticipation = oldemovector.EmotionalTone[7].Value;
-			}
-			foreach (string nw in newWords)
-			{
-				Vector newvector = new Vector
-				{
-					Word = nw,
+                oldvector.Trust = oldemovector.EmotionalTone[1].Value;
+                oldvector.Fear = oldemovector.EmotionalTone[2].Value;
+                oldvector.Surprise = oldemovector.EmotionalTone[3].Value;
+                oldvector.Sadness = oldemovector.EmotionalTone[4].Value;
+                oldvector.Disgust = oldemovector.EmotionalTone[5].Value;
+                oldvector.Anger = oldemovector.EmotionalTone[6].Value;
+                oldvector.Anticipation = oldemovector.EmotionalTone[7].Value;
+            }
+            foreach (string nw in newWords)
+            {
+                Vector newvector = new Vector
+                {
+                    Word = nw,
                     Priority = 0.5,
-					Joy = sum.EmotionalTone[0].Value,
-					Trust = sum.EmotionalTone[1].Value,
-					Fear = sum.EmotionalTone[2].Value,
-					Surprise = sum.EmotionalTone[3].Value,
-					Sadness = sum.EmotionalTone[4].Value,
-					Disgust = sum.EmotionalTone[5].Value,
-					Anger = sum.EmotionalTone[6].Value,
-					Anticipation = sum.EmotionalTone[7].Value
-				};
-				context.Vectors.Add(newvector);
-			}
-			context.SaveChanges();
-			return sum;
-		}
+                    Joy = sum.EmotionalTone[0].Value,
+                    Trust = sum.EmotionalTone[1].Value,
+                    Fear = sum.EmotionalTone[2].Value,
+                    Surprise = sum.EmotionalTone[3].Value,
+                    Sadness = sum.EmotionalTone[4].Value,
+                    Disgust = sum.EmotionalTone[5].Value,
+                    Anger = sum.EmotionalTone[6].Value,
+                    Anticipation = sum.EmotionalTone[7].Value
+                };
+                context.Vectors.Add(newvector);
+            }
+            context.SaveChanges();
+            return sum;
+        }
 
-		private List<string> ParseTextByWord(string text)
-		{
-			List<string> result = new List<string>();
+        private List<string> ParseTextByWord(string text)
+        {
+            List<string> result = new List<string>();
 
-			string[] words = text.Split(new char[]{' ', ',',
-					'.', '-', '"', '(', ')', ';', ':', '?', '!'});
-			foreach (var word in words)
-			{
-				if (!result.Contains(word))
-					result.Add(word);
-			}
-			return result;
-		}
+            string[] words = text.Split(new char[]{' ', ',',
+                    '.', '-', '"', '(', ')', ';', ':', '?', '!'});
+            foreach (var word in words)
+            {
+                if (!result.Contains(word))
+                    result.Add(word);
+            }
+            return result;
+        }
 
-        private EmotionalVector countSum(String text,List<EmotionalVector> dbVectors)
+        private EmotionalVector countSum(String text, List<EmotionalVector> dbVectors)
         {
             EmotionalVector sum = new EmotionalVector(text, 0, 0, 0, 0, 0, 0, 0, 0, 0);
             double sumPriority = 0;
@@ -132,6 +135,24 @@ namespace Islam.Service
                 dbvector.Priority = 1;
             }
             context.SaveChanges();
+        }
+
+        private List<string> checkStopWords(List<string> words)
+        {
+            string line;
+            List<string> stopWords = new List<string>();
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\volkov\Desktop\emotion\stopwords\stopwords.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                stopWords.Add(line);
+            }
+            file.Close();
+            foreach(string word in words)
+            {
+                if (stopWords.Contains(word)) words.Remove(word);
+            }
+
+            return words;
         }
     }
 }
